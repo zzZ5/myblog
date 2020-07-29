19985f5860a63c9154a04ff8a0ea412a4c886efd
zzZ5 <baoju_liu@foxmail.com>
2020-07-29 16:21:55 +0800

    去除url中的硬编码

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('articles/<int:id>/', views.article),
-    path('categories/<str:category>/', views.category),
-    path('tags/<str:tag>/', views.tag),
-    path('search/', views.search),
+    path('articles/<int:id>/', views.article, name='articles'),
+    path('categories/<str:category>/', views.category, name='categories'),
+    path('tags/<str:tag>/', views.tag, name='tags'),
+    path('search/', views.search, name='search'),
 ]
